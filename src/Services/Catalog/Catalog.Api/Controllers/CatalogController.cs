13f22e64433a197b99a455cabb3383b8e5c1f9a6
Saiful Islam <saifulprogrammer@gmail.com>
2022-06-18 14:15:15 +0600

    added dependency injection

---
+++ src/Services/Catalog/Catalog.Api/Controllers/CatalogController.cs
@@ -0,0 +1,83 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Catalog.Api.Repositories;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace Catalog.Api.Controllers
+{
+    
+    public class CatalogController : BaseController
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ILogger<CatalogController> _logger;
+        public CatalogController(IProductRepository productRepository,
+            ILogger<CatalogController> logger)
+        {
+            _productRepository = productRepository;
+            _logger = logger;
+        }
+
+        [HttpGet("GetProducts")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProducts();
+            return Ok(products);
+        }
+        [HttpGet("id:length(24)", Name = "GetProductById")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductById(string id)
+        {
+            var product = await _productRepository.GetProduct(id);
+            if(product == null)
+            {
+                _logger.LogError($"product {id} not fount");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [Route("[action]/{category}", Name = "GetProductByCategory")]
+        [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+        {
+            var product = await _productRepository.GetProductByCategory(category);
+            if (product == null)
+            {
+                _logger.LogError($"product with category {category} not fount");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> CreateProduct([FromBody] Product product)
+        {
+            await _productRepository.CreateProduct(product);
+
+            return CreatedAtRoute("GetProductById", new { id = product.Id }, product);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> UpdateProduct([FromBody] Product product)
+        {
+            return Ok(await _productRepository.UpdateProduct(product));
+        }
+
+        [HttpDelete("id:length(24)", Name = "DeleteProductById")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> DeleteProductById(string id)
+        {
+            return Ok(await _productRepository.DeleteProduct(id));
+        }
+    }
+}
