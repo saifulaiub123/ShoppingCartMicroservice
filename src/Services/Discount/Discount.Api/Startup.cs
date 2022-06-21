f8f5fb517f1e8a90e74ac293c3a39d17dbe4683f
Saiful Islam <saifulprogrammer@gmail.com>
2022-06-21 10:11:30 +0600

    finished discount api as mictoservice and added docker containerization

--- src/Services/Discount/Discount.Api/Startup.cs
+++ src/Services/Discount/Discount.Api/Startup.cs
@@ -1,3 +1,4 @@
+using Discount.Api.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -31,6 +32,7 @@ namespace Discount.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.Api", Version = "v1" });
             });
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
