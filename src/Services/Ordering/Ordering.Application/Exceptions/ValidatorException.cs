dfd97006c2f273d840802c963e6e13b5cd305677
Saiful Islam <saifulprogrammer@gmail.com>
2022-06-26 16:29:37 +0600

    Added custom exception and return

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidatorException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.Results;
+
+namespace Ordering.Application.Exceptions
+{
+    public class ValidatorException : ApplicationException
+    {
+        public ValidatorException(string message) : base("One or more validation failures have occured.")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+        public ValidatorException(IEnumerable<ValidationFailure> failures) : base()
+        {
+            Errors = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+        }
+
+        public IDictionary<string, string[]> Errors { get; set; }
+    }
+}
