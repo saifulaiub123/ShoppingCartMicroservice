f5209c6823902fe6c56527c5103de194fac8eef3
Saiful Islam <saifulprogrammer@gmail.com>
2022-07-05 10:55:57 +0000

    added dependency, controller

--- src/Services/Ordering/Ordering.Api/Startup.cs
+++ src/Services/Ordering/Ordering.Api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ordering.Application;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +32,7 @@ namespace Ordering.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ordering.Api", Version = "v1" });
             });
+            services.AddApplicationServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ src/Services/Ordering/Ordering.Infrastructure/InfraStructureServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Ordering.Infrastructure
+{
+    public static class InfraStructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            //services.AddDbContext<OrderContext>(options =>
+            //    options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
+
+            //services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            //services.AddScoped<IOrderRepository, OrderRepository>();
+
+            //services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            //services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
